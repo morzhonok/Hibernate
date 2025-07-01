7a8d4aa89203cc3b6ac6a349406875b198d8d603
morzhonok <morzhonok.rozovatyy.10@mail.ru>
2025-07-01 14:25:41 +0300

    comit 11

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,18 +1,14 @@
 package jm.task.core.jdbc.util;
 
-import com.sun.istack.logging.Logger;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
 
 public final class Util {
 
     public static final String USERNAME = "root";
     public static final String PASSWORD = "root";
     public static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
-    private static final Logger LOGGER = Logger.getLogger(Util.class.getName().getClass());
 
     public Util() {
     }
@@ -21,7 +17,6 @@ public final class Util {
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
-            LOGGER.log(Level.INFO, "Не удалось загрузить класс драйвера", e);
             throw new RuntimeException("Ошибка подключения к БД", e);
         }
     }
